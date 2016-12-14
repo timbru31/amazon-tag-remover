b6d3b835d1530c260c282f75481135827ceb72c3
Tim Brust <tim.brust@sinnerschrader.com>
2016-12-14 15:24:08 +0100

    refactor: use classList toggle, const and textContent

--- js/frontend.js
+++ js/frontend.js
@@ -1,15 +1,15 @@
 'use strict';
 
 chrome.runtime.onMessage.addListener((request, sender, resetTag) => {
-  let div = document.createElement('div');
+  const div = document.createElement('div');
   div.className = 'amazon-tag-remover';
-  div.innerText = `The following tag was found and has been removed: ${request.tag}`;
+  div.textContent = `The following tag was found and has been removed: ${request.tag}`;
   document.body.appendChild(div);
-  div.classList.add('amazon-tag-remover-fade-in');
+  div.classList.toggle('amazon-tag-remover-fade-in');
 
   setTimeout(() => {
-    div.classList.remove('amazon-tag-remover-fade-in');
-    div.classList.add('amazon-tag-remover-fade-out');
+    div.classList.toggle('amazon-tag-remover-fade-in');
+    div.classList.toggle('amazon-tag-remover-fade-out');
   }, 7000);
   resetTag();
 });
