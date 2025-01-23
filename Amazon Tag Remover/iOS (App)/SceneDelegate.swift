3182dbb30fd46b9107d284e958604efe829a6583
Tim Brust <tim.brust@accenture.com>
2025-01-23 21:48:15 +0100

    style: convert to spaces

--- Amazon Tag Remover/iOS (App)/SceneDelegate.swift
+++ Amazon Tag Remover/iOS (App)/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-		guard (scene as? UIWindowScene) != nil else { return }
+        guard (scene as? UIWindowScene) != nil else { return }
     }
 
 }
